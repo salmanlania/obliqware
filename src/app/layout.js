92e8f828755e9cce548fb1defc04e5090e0ef8d0
Salman Lania <salmanlania@gmail.com>
2024-11-11 09:53:38 +0500

    new scriptvgcj

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,57 +23,68 @@ import Head from 'next/head';
 import "@/assets/css/tolak.css";
 import "@/assets/css/tolak-dark.css";
 import { usePathname } from "next/navigation";
-
 import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-  const [themeState, setThemeState] = useState("light");
+    const pathname = usePathname();
+    const [themeState, setThemeState] = useState("light");
 
-  useEffect(() => {
-    if (pathname === "/-dark" || pathname === "/home2-dark" || pathname === "/home3-dark" || pathname === "/home4-dark") {
-      setThemeState("dark");
-    } else {
-      setThemeState("light");
-    }
-  }, [pathname]);
+    useEffect(() => {
+        if (pathname === "/-dark" || pathname === "/home2-dark" || pathname === "/home3-dark" || pathname === "/home4-dark") {
+            setThemeState("dark");
+        } else {
+            setThemeState("light");
+        }
+    }, [pathname]);
 
-  return (
-    <html lang='en'>
-      <Head></Head>
-      {/* Google Analytics Script */}
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=G-F22MLNCZ7T`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+    const trackPageView = (url) => {
+        if (window.gtag) {
+            window.gtag('config', 'G-F22MLNCZ7T', { page_path: url });
+            window.gtag('config', 'AW-16761372846', { page_path: url });
+        }
+    };
+
+    // Track pageview on route change
+    useEffect(() => {
+        trackPageView(pathname);
+    }, [pathname]);
+
+    return (
+        <html lang='en'>
+            <Head></Head>
+            {/* Google Analytics Script */}
+            <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=G-F22MLNCZ7T`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-F22MLNCZ7T');
           `}
-      </Script>
+            </Script>
 
-      {/* Google Ads Script */}
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=AW-16761372846`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-ads" strategy="afterInteractive">
-        {`
+            {/* Google Ads Script */}
+            <Script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=AW-16761372846`}
+                strategy="afterInteractive"
+            />
+            <Script id="google-ads" strategy="afterInteractive">
+                {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-16761372846');
           `}
-      </Script>
+            </Script>
 
-      <body className={`custom- cursor ${pathname === "/home5" ? 'home5' : pathname === "/home5-one" ? 'home5' : pathname === "/home6" ? "home6" : pathname === "/home6-one" ? "home6" : pathname === "/home7" ? 'home7' : pathname === "/home7-one" ? 'home7' : pathname === "/home-boxed" ? "boxed-wrapper" : ''} ${themeState}`}>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html >
-  );
+            <body className={`custom- cursor ${pathname === "/home5" ? 'home5' : pathname === "/home5-one" ? 'home5' : pathname === "/home6" ? "home6" : pathname === "/home6-one" ? "home6" : pathname === "/home7" ? 'home7' : pathname === "/home7-one" ? 'home7' : pathname === "/home-boxed" ? "boxed-wrapper" : ''} ${themeState}`}>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html >
+    );
 }
